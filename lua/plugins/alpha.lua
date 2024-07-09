865a524fdf906a06dca09ee88952196ae1e6a518
Levi <udaybindal2312@gmail.com>
2024-07-09 22:54:39 +0530

    config/nvim: add alpha.nvim for stylish greeter
    * also add window borders for snippets

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,36 @@
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.startify")
+
+    dashboard.section.header.val = {
+      -- [[                                                                       ]],
+      -- [[                                                                       ]],
+      -- [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                              @iTZLEVI2312                             ]],
+      [[                                                                       ]],
+    }
+
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
+}
